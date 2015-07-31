ea6b6bd6e28080191a8857766f231888c8772ce8
Natalie Chin <natalie.j.chin@gmail.com>
2015-07-30 22:41:15 -0500

    Add if statement that checks whether there are any skills in the bio object. Append skills header and list skills if true.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -83,3 +83,20 @@ var bio = {
         }
     ]
 }
+
+if (bio.skills.length > 0) {
+   $("#header").append(HTMLskillsStart);
+   
+   
+   var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+   $("#skills").append(formattedSkill);
+   
+   formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+   $("#skills").append(formattedSkill);
+   
+   formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+   $("#skills").append(formattedSkill);
+   
+   formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+   $("#skills").append(formattedSkill);
+ }
